4dd57a99577ba4bcd9e66a6ea5954f908313ebdc
Gireeshsuresh <gireesh.suresh@zenuity.com>
2020-04-06 19:10:33 -0400

    Add Misc

---
+++ ProgrammingLanguages/Python/Sample.py
@@ -0,0 +1,11 @@
+# /**
+#  * @author  [Gireesh Suresh]
+#  * @email   [gireesh.star@gmail.com]
+#  * @create  date 2020-04-06 18:51:33
+#  * @modify  date 2020-04-06 18:51:33
+#  * @desc    [description]
+#  */
+
+testSting = "Hello World"
+print(testSting)
+
