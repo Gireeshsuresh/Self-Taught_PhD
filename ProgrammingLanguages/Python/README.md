863b287a47ae7208a905596c415b1608ac5a8368
Gireeshsuresh <gireesh.suresh@zenuity.com>
2020-04-06 19:46:26 -0400

    Add Sample Python file

---
+++ ProgrammingLanguages/Python/README.md
@@ -0,0 +1,3 @@
+## Python Tutorials / Courses:
+1. Kaggle [[Beginner Level]](https://www.kaggle.com/learn/python)
+2. 
--- ProgrammingLanguages/Python/Sample.py
+++ ProgrammingLanguages/Python/Sample.py
@@ -3,9 +3,31 @@
 #  * @email   [gireesh.star@gmail.com]
 #  * @create  date 2020-04-06 18:51:33
 #  * @modify  date 2020-04-06 18:51:33
-#  * @desc    [description]
+#  * @desc    [Rough work]
 #  */
 
 testSting = "Hello World"
 print(testSting)
+
+spam_amount = 0
+print(spam_amount)
+
+# Ordering Spam, egg, Spam, Spam, bacon and Spam (4 more servings of Spam)
+spam_amount = spam_amount + 4
+
+if spam_amount > 0:
+    print("But I don't want ANY spam!")
+
+viking_song = "Spam " * spam_amount
+print(viking_song)
+
+type(19.95) # float
+
+print(min(1, 2, 3))
+print(max(1, 2, 3))
+
+print(float(10))
+print(int(3.33))
+# They can even be called on strings!
+print(int('807') + 1)
 
