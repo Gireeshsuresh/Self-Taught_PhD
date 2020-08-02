b37129c3346b08e3c2d21392605f0baaa4381552
Gireesh Suresh <gireesh.star@gmail.com>
2020-08-02 15:21:07 -0400

    Updated the Computer Vision Package List
    - Updated the package list for Computer Vision environment.
    - Added the Scripts to create package list and install from a given package list.

---
+++ DeepLearning/Anaconda_PackageList/create_packageList.sh
@@ -0,0 +1,5 @@
+#  conda list : lists the packages in all the environments 
+# -n followed by the name of the anaconda env whose package list you want to export to the text file. (ex) -n myenv . // Here 'myenv' is the name of the env and is specified by the '-n' flag.
+#  The 'blah.txt' is the name of the text file which consists of the list of packages that was exported from the given environment.
+
+conda list -n cv --export > ./cv_env_packageList.txt
---
+++ DeepLearning/Anaconda_PackageList/install_packages.sh
@@ -0,0 +1,1 @@
+conda create -n cv --file /home/omg/workspace/Self-Taught_PhD/DeepLearning/Anaconda_PackageList/cv_env_packageList.txt
