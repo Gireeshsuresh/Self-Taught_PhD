8d76d2b5e9a1fba7f0c31b02f571550e21f77936
Gireesh Suresh <gireesh.star@gmail.com>
2020-08-09 19:20:51 -0400

    Updated the Anaconda CV Package List
    - Add the install script file

--- DeepLearning/Anaconda_PackageList/install_packages.sh
+++ DeepLearning/Anaconda_PackageList/install_packages.sh
@@ -1,2 +1,2 @@
 conda create --name cv python=3
-conda install --name cv --file /home/omg/workspace/Self-Taught_PhD/DeepLearning/Anaconda_PackageList/cv_env_packageList.txt
+conda install --name cv --file /home/omg/workspace/Self-Taught_PhD/DeepLearning/Anaconda_PackageList/cv_env_packageList_python_3.6.txt
