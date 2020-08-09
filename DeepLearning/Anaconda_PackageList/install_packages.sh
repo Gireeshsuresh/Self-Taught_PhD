4b4d862796ad15fde5edcb3ef901fc630cad139d
Gireesh Suresh <gireesh.star@gmail.com>
2020-08-09 19:17:46 -0400

    Updated the Anaconda CV Package List
    - Updated the Computer Vision Anaconda Package list based on Python=3.6.

--- DeepLearning/Anaconda_PackageList/install_packages.sh
+++ DeepLearning/Anaconda_PackageList/install_packages.sh
@@ -1,1 +1,2 @@
-conda create -n cv --file /home/omg/workspace/Self-Taught_PhD/DeepLearning/Anaconda_PackageList/cv_env_packageList.txt
+conda create --name cv python=3
+conda install --name cv --file /home/omg/workspace/Self-Taught_PhD/DeepLearning/Anaconda_PackageList/cv_env_packageList.txt
