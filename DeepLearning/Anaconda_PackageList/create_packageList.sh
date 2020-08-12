5aaaba9ff4a0dbbc66997c94806b8439841e4f13
Gireesh Suresh <gireesh.star@gmail.com>
2020-08-12 12:00:59 -0400

    Update the Script Files for Anaconda

--- DeepLearning/Anaconda_PackageList/create_packageList.sh
+++ DeepLearning/Anaconda_PackageList/create_packageList.sh
@@ -2,4 +2,4 @@
 # -n followed by the name of the anaconda env whose package list you want to export to the text file. (ex) -n myenv . // Here 'myenv' is the name of the env and is specified by the '-n' flag.
 #  The 'blah.txt' is the name of the text file which consists of the list of packages that was exported from the given environment.
 
-conda list -n cv --export > ./cv_env_packageList_python_3.6.txt
+conda list -n cv --explicit > ./cv_env_packageList_python_3.8.5.txt
