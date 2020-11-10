3de2897e42d80a89853d4c4aac9b5233ae3d31bf
Gireeshsuresh <gireesh.star@gmail.com>
2020-11-09 21:07:24 -0500

    Add Snipets PythonChallenge.com

---
+++ CodingPractice/PythonChallenge.com/L0.py
@@ -0,0 +1,2 @@
+val= 2**38
+#Replace the 0 in html with Val to go to next level
---
+++ CodingPractice/PythonChallenge.com/L1.py
@@ -0,0 +1,7 @@
+raw = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle grgl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
+
+table = str.maketrans("abcdefghijklmnopqrstuvwxyz", "cdefghijklmnopqrstuvwxyzab")
+
+result = raw.translate(table)
+
+print(result)
