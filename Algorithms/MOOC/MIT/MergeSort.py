1685046fa7ef4d09ca29c82511425cd3ca58d72e
Gireeshsuresh <gireesh.star@gmail.com>
2020-11-10 20:50:18 -0500

    Add Mooc Stuff: MIT Intro to Algo
    - Add MergeSort.py

---
+++ Algorithms/MOOC/MIT/MergeSort.py
@@ -0,0 +1,35 @@
+def merge_sort(arr):
+    if len(arr)>1:
+        mid = len(arr)//2
+        L = arr[:mid]
+        R = arr[mid:]
+
+        merge_sort(L)
+        merge_sort(R)
+
+        i = 0
+        j = 0
+        k = 0
+        while i<len(L) and j<len(R):
+            if L[i] < R[j]:
+                arr[k]=L[i]
+                i+=1
+            else:
+                arr[k]=R[j]
+                j+=1
+            k+=1
+        while i < len(L): 
+            arr[k] = L[i] 
+            i+= 1
+            k+= 1
+          
+        while j < len(R): 
+            arr[k] = R[j] 
+            j+= 1
+            k+= 1
+if __name__ == "__main__":
+    arr = [8,7,6,5,4,3,2,1]
+    print("List = {}".format(arr))
+    merge_sort(arr)
+    print("List = {}".format(arr))
+
