422f91e04782a979e009e69290693084f4e37522
Gireesh Suresh <gireesh.star@gmail.com>
2020-11-15 19:46:31 -0500

    Add Code to LeetCode Series
    - Container with Most Water

---
+++ CodingPractice/LeetCode/container_with_most_water.py
@@ -0,0 +1,28 @@
+from typing import List
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        start = 0
+        end = len(height) -1
+        max_area = 0
+        local_area = 0
+        
+        while start != end: 
+            # area = max(area, min(height[start], height[end]) * (end - start))         
+            if height[start] < height[end]:
+                local_area = height[start] * (end - start)
+                start += 1
+            else: 
+                local_area = height[end] * (end - start)
+                end -= 1
+            if local_area > max_area:
+                max_area = local_area
+            
+        return max_area 
+
+if __name__ == "__main__":
+    sample_sol = Solution()
+
+    height = [1,8,6,2,5,4,8,3,7]
+    output_len = sample_sol.maxArea(height)
+    print("Output = {}".format(output_len))
