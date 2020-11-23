1b0b19213300d19de7b627d259cb1e52a424c72e
Gireesh Suresh <gireesh.star@gmail.com>
2020-11-22 21:07:19 -0500

    Add Code to Leet Code Series
    - Add the Test file

---
+++ CodingPractice/LeetCode/test.py
@@ -0,0 +1,10 @@
+from typing import List
+class Solution:
+
+if __name__ == "__main__":
+    mySolution = Solution()
+
+    nums1 = [10,2,5,3]
+    print("Input  : {}".format(nums1))
+   
+    print("Output : {}".format(out))
