6653eeb8bd866cb103c2ec8f0541fa41a8f44ddd
Gireesh Suresh <gireesh.star@gmail.com>
2021-01-08 22:56:34 -0500

    Add Code to Leet Code Series
    - Two Sum

--- CodingPractice/LeetCode/TwoSum.py
+++ CodingPractice/LeetCode/TwoSum.py
@@ -1,19 +1,23 @@
 from typing import List
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        out = []
+        out = [-1,-1]
         for idx,num in enumerate(nums):
-            print(idx, num)
-        
+            y = target - num
+            if y in nums[idx+1:]:
+                out[0] = idx
+                out[1] = nums.index(y, idx+1)
+                break
         return out
+
 
 
 
 if __name__ == "__main__":
     mySolution = Solution()
     
-    nums = [10,2,5,3]
-    target = 13
+    nums = [3,2,4]
+    target = 6
 
     out = mySolution.twoSum(nums, target)
     print("Input  : {}".format(nums))
