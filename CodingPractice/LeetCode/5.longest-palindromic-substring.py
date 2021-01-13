f8db2164eb39cb65f42236a9bed4f1ad8a7209e7
Gireeshsuresh <gireesh.star@gmail.com>
2021-01-12 21:34:18 -0500

    Add Code to Leet Code Series
    - Longest Pallindromic Substring

---
+++ CodingPractice/LeetCode/5.longest-palindromic-substring.py
@@ -0,0 +1,63 @@
+#
+# @lc app=leetcode id=5 lang=python3
+#
+# [5] Longest Palindromic Substring
+#
+# https://leetcode.com/problems/longest-palindromic-substring/description/
+#
+# algorithms
+# Medium (30.12%)
+# Likes:    9215
+# Dislikes: 622
+# Total Accepted:    1.2M
+# Total Submissions: 3.8M
+# Testcase Example:  '"babad"'
+#
+# Given a string s, return the longest palindromic substring in s.
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: s = "babad"
+# Output: "bab"
+# Note: "aba" is also a valid answer.
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: s = "cbbd"
+# Output: "bb"
+# 
+# 
+# Example 3:
+# 
+# 
+# Input: s = "a"
+# Output: "a"
+# 
+# 
+# Example 4:
+# 
+# 
+# Input: s = "ac"
+# Output: "a"
+# 
+# 
+# 
+# Constraints:
+# 
+# 
+# 1 <= s.length <= 1000
+# s consist of only digits and English letters (lower-case and/or upper-case),
+# 
+# 
+#
+
+# @lc code=start
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        
+# @lc code=end
+
