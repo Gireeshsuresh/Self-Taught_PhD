41e1a799dce8d65279b3d64974391f1221c24ffb
Gireesh Suresh <gireesh.star@gmail.com>
2021-03-07 11:22:35 -0500

    Add Code to Leet Code Series
    - MedianOfTwoSortedArrays

---
+++ CodingPractice/LeetCode/MedianOfTwoSortedArrays.py
@@ -0,0 +1,14 @@
+from typing import List
+class Solution:
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        out = 0.00
+        return out
+if __name__ == "__main__":
+    mySolution = Solution()
+
+    nums1 = [10,2,5,3]
+    nums2 = [10,2,5,3]
+    print("Input  : L1 -  {} , L2 - {} ".format(nums1, nums2))
+    out = mySolution.findMedianSortedArrays(nums1, nums2)
+    print("Output : {}".format(out))
