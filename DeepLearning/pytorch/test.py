f2d46b6766fd4e223a86ae2e6759f41bf3b779fb
Gireesh Suresh <gireesh.star@gmail.com>
2021-03-07 11:21:29 -0500

    Add Pytorch Folder
    - Add Pytorch Folder

---
+++ DeepLearning/pytorch/test.py
@@ -0,0 +1,7 @@
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor, Lambda, Compose
+import matplotlib.pyplot as plt
+
