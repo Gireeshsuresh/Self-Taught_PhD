360d64ed1b946f7afee25c34bfe5f1e68e3642d0
Gireesh Suresh <gireesh.star@gmail.com>
2021-10-12 14:41:16 +0000

    Q)704 - Binary Search | LeetCode Series

--- CodingPractice/LeetCode/Q704_binary_search.py
+++ CodingPractice/LeetCode/Q704_binary_search.py
@@ -1,22 +1,24 @@
 from typing import List
 
-def binary_search(nums:List, Target:int):
-    if Target not in nums:
-        return -1
-    else:
-        left = 0
-        mid_idx = len(nums)//2
-        mid_val = nums[mid_idx]
-        if Target == mid_val:
-            return mid_idx
-        elif Target < mid_val:
-            return binary_search(nums[0:mid_idx], Target)
-        else:
-            return binary_search(nums[mid_idx+1:len(nums)], Target)            
-
-# if __name__ == 'main':
+class Solution:
+    def binary_search(self, nums: List[int], target: int) -> int:
+      if target not in nums:
+          return -1
+      else:
+          left = 0
+          mid_idx = len(nums)//2
+          mid_val = nums[mid_idx]
+          if target == mid_val:
+              return mid_idx
+          elif target < mid_val:
+              return self.binary_search(nums[0:mid_idx], target)
+          else:
+              return self.binary_search(nums[mid_idx+1:len(nums)], target)            
+ 
+ 
+mySolution = Solution()
 list = [2,3,4,10,40]
 x = 10
-r = binary_search(list, x)
-print(f'Resulting Index = {r}')
+result = mySolution.binary_search(list, x)
+print(f'Resulting Index = {result}')
     
