1f9c54e6383e9974f3fb2886dc7b444c7c0bb1c0
Gireesh Suresh <gireesh.star@gmail.com>
2021-10-12 18:05:27 +0000

    Q)704 - Binary Search | LeetCode Series

--- CodingPractice/LeetCode/Q704_binary_search.py
+++ CodingPractice/LeetCode/Q704_binary_search.py
@@ -2,19 +2,19 @@ from typing import List
 
 class Solution:
     def binary_search(self, nums: List[int], target: int) -> int:
-      if target not in nums:
-          return -1
-      else:
-          left = 0
-          mid_idx = len(nums)//2
-          mid_val = nums[mid_idx]
-          if target == mid_val:
-              return mid_idx
-          elif target < mid_val:
-              return self.binary_search(nums[0:mid_idx], target)
-          else:
-              return self.binary_search(nums[mid_idx+1:len(nums)], target)            
- 
+      left = 0
+      right = len(nums) - 1
+      while left <= right:
+        # https://ai.googleblog.com/2006/06/extra-extra-read-all-about-it-nearly.html
+        pivot = left + (right - left)//2
+        pivot_val = nums[pivot]
+        if target == pivot_val:
+          return pivot
+        elif target < pivot_val:
+          right = pivot - 1
+        else:
+          left = pivot + 1
+      return -1
  
 mySolution = Solution()
 list = [2,3,4,10,40]
