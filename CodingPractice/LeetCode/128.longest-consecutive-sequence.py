098ce749147f29c1eff7ec025e6a853f4ac22278
gireeshsuresh <gireesh.star@gmail.com>
2025-03-16 18:53:06 -0400

    LC | Array and Hashing
    - Longest Consecutive Sequence

---
+++ CodingPractice/LeetCode/128.longest-consecutive-sequence.py
@@ -0,0 +1,74 @@
+#
+# @lc app=leetcode id=128 lang=python3
+#
+# [128] Longest Consecutive Sequence
+#
+# https://leetcode.com/problems/longest-consecutive-sequence/description/
+#
+# algorithms
+# Medium (47.17%)
+# Likes:    21083
+# Dislikes: 1124
+# Total Accepted:    2.4M
+# Total Submissions: 5.1M
+# Testcase Example:  '[100,4,200,1,3,2]'
+#
+# Given an unsorted array of integers nums, return the length of the longest
+# consecutive elements sequence.
+# 
+# You must write an algorithm that runs in O(n) time.
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: nums = [100,4,200,1,3,2]
+# Output: 4
+# Explanation: The longest consecutive elements sequence is [1, 2, 3, 4].
+# Therefore its length is 4.
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: nums = [0,3,7,2,5,8,4,6,0,1]
+# Output: 9
+# 
+# 
+# Example 3:
+# 
+# 
+# Input: nums = [1,0,1,2]
+# Output: 3
+# 
+# 
+# 
+# Constraints:
+# 
+# 
+# 0 <= nums.length <= 10^5
+# -10^9 <= nums[i] <= 10^9
+# 
+# 
+#
+
+from typing import List
+# @lc code=start
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        numSet = set(nums)
+        longest = 0
+
+        for num in numSet:
+            if (num - 1) not in numSet:
+                length = 1
+                while (num + length) in numSet:
+                    length += 1
+                longest = max(length, longest)
+        return longest
+        
+# @lc code=end
+
+s = Solution()
+input = [9,1,4,7,3,-1,0,5,8,-1,6]
+print(s.longestConsecutive(input))
