83b78aab137d88b3182f1b1d075bd115cd56ddd0
gireeshsuresh <gireesh.star@gmail.com>
2025-03-29 16:02:18 -0400

    LC | Binary Search
    - Time based key value store

---
+++ CodingPractice/LeetCode/981.time-based-key-value-store.py
@@ -0,0 +1,106 @@
+#
+# @lc app=leetcode id=981 lang=python3
+#
+# [981] Time Based Key-Value Store
+#
+# https://leetcode.com/problems/time-based-key-value-store/description/
+#
+# algorithms
+# Medium (49.38%)
+# Likes:    4979
+# Dislikes: 664
+# Total Accepted:    562K
+# Total Submissions: 1.1M
+# Testcase Example:  '["TimeMap","set","get","get","set","get","get"]\n' +
+  '[[],["foo","bar",1],["foo",1],["foo",3],["foo","bar2",4],["foo",4],["foo",5]]'
+#
+# Design a time-based key-value data structure that can store multiple values
+# for the same key at different time stamps and retrieve the key's value at a
+# certain timestamp.
+# 
+# Implement the TimeMap class:
+# 
+# 
+# TimeMap() Initializes the object of the data structure.
+# void set(String key, String value, int timestamp) Stores the key key with the
+# value value at the given time timestamp.
+# String get(String key, int timestamp) Returns a value such that set was
+# called previously, with timestamp_prev <= timestamp. If there are multiple
+# such values, it returns the value associated with the largest timestamp_prev.
+# If there are no values, it returns "".
+# 
+# 
+# 
+# Example 1:
+# 
+# 
+# Input
+# ["TimeMap", "set", "get", "get", "set", "get", "get"]
+# [[], ["foo", "bar", 1], ["foo", 1], ["foo", 3], ["foo", "bar2", 4], ["foo",
+# 4], ["foo", 5]]
+# Output
+# [null, null, "bar", "bar", null, "bar2", "bar2"]
+# 
+# Explanation
+# TimeMap timeMap = new TimeMap();
+# timeMap.set("foo", "bar", 1);  // store the key "foo" and value "bar" along
+# with timestamp = 1.
+# timeMap.get("foo", 1);         // return "bar"
+# timeMap.get("foo", 3);         // return "bar", since there is no value
+# corresponding to foo at timestamp 3 and timestamp 2, then the only value is
+# at timestamp 1 is "bar".
+# timeMap.set("foo", "bar2", 4); // store the key "foo" and value "bar2" along
+# with timestamp = 4.
+# timeMap.get("foo", 4);         // return "bar2"
+# timeMap.get("foo", 5);         // return "bar2"
+# 
+# 
+# 
+# Constraints:
+# 
+# 
+# 1 <= key.length, value.length <= 100
+# key and value consist of lowercase English letters and digits.
+# 1 <= timestamp <= 10^7
+# All the timestamps timestamp of set are strictly increasing.
+# At most 2 * 10^5 calls will be made to set and get.
+# 
+# 
+#
+
+# @lc code=start
+class TimeMap:
+
+  def __init__(self):
+    # Dictionary to store key -> list of (timestamp, value) pairs
+    self.store = {}
+
+  def set(self, key: str, value: str, timestamp: int) -> None:
+    if key not in self.store:
+      self.store[key] = []
+    # Append the (timestamp, value) pair to the list
+    self.store[key].append((timestamp, value))
+
+  def get(self, key: str, timestamp: int) -> str:
+    if key not in self.store:
+      return ""
+    # Perform binary search to find the largest timestamp <= given timestamp
+    values = self.store[key]
+    left, right = 0, len(values) - 1
+    res = ""
+    while left <= right:
+      mid = (left + right) // 2
+      if values[mid][0] <= timestamp:
+        res = values[mid][1]  # Update result
+        left = mid + 1  # Search in the right half
+      else:
+        right = mid - 1  # Search in the left half
+    return res
+
+
+# Your TimeMap object will be instantiated and called as such:
+# obj = TimeMap()
+# obj.set(key,value,timestamp)
+# param_2 = obj.get(key,timestamp)
+# @lc code=end
+
