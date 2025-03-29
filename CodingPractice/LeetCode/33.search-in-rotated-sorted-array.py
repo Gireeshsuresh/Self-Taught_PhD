4aa9b1da83e04f1688e851f46345659f17088f5f
gireeshsuresh <gireesh.star@gmail.com>
2025-03-29 15:59:34 -0400

    LC | Binary Search
    - Find min in a rotated sorted array

---
+++ CodingPractice/LeetCode/33.search-in-rotated-sorted-array.py
@@ -0,0 +1,82 @@
+#
+# @lc app=leetcode id=33 lang=python3
+#
+# [33] Search in Rotated Sorted Array
+#
+# https://leetcode.com/problems/search-in-rotated-sorted-array/description/
+#
+# algorithms
+# Medium (42.44%)
+# Likes:    27686
+# Dislikes: 1683
+# Total Accepted:    3.4M
+# Total Submissions: 8.1M
+# Testcase Example:  '[4,5,6,7,0,1,2]\n0'
+#
+# There is an integer array nums sorted in ascending order (with distinct
+# values).
+# 
+# Prior to being passed to your function, nums is possibly rotated at an
+# unknown pivot index k (1 <= k < nums.length) such that the resulting array is
+# [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]]
+# (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3
+# and become [4,5,6,7,0,1,2].
+# 
+# Given the array nums after the possible rotation and an integer target,
+# return the index of target if it is in nums, or -1 if it is not in nums.
+# 
+# You must write an algorithm with O(log n) runtime complexity.
+# 
+# 
+# Example 1:
+# Input: nums = [4,5,6,7,0,1,2], target = 0
+# Output: 4
+# Example 2:
+# Input: nums = [4,5,6,7,0,1,2], target = 3
+# Output: -1
+# Example 3:
+# Input: nums = [1], target = 0
+# Output: -1
+# 
+# 
+# Constraints:
+# 
+# 
+# 1 <= nums.length <= 5000
+# -10^4 <= nums[i] <= 10^4
+# All values of nums are unique.
+# nums is an ascending array that is possibly rotated.
+# -10^4 <= target <= 10^4
+# 
+# 
+#
+from typing import List
+# @lc code=start
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] == target:
+                return m
+            
+            # Determine the sorted half
+            if nums[l] <= nums[m]:  # Left half is sorted
+                if nums[l] <= target < nums[m]:
+                    r = m - 1
+                else:
+                    l = m + 1
+            else:  # Right half is sorted
+                if nums[m] < target <= nums[r]:
+                    l = m + 1
+                else:
+                    r = m - 1
+        
+        return -1
+        
+# @lc code=end
+
+s = Solution()
+nums = [4,5,6,7,0,1,2]
+target = 0
+print(s.search(nums, target))
