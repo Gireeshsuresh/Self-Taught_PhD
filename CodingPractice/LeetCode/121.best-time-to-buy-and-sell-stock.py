324d55287eec90f2f8a58886385735f24c7f0142
gireeshsuresh <gireesh.star@gmail.com>
2025-03-29 21:02:00 -0400

    LC |  Sliding Window
    - Best time to buy and sell stock

---
+++ CodingPractice/LeetCode/121.best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,72 @@
+#
+# @lc app=leetcode id=121 lang=python3
+#
+# [121] Best Time to Buy and Sell Stock
+#
+# https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+#
+# algorithms
+# Easy (54.86%)
+# Likes:    32827
+# Dislikes: 1265
+# Total Accepted:    6.1M
+# Total Submissions: 11.1M
+# Testcase Example:  '[7,1,5,3,6,4]'
+#
+# You are given an array prices where prices[i] is the price of a given stock
+# on the i^th day.
+# 
+# You want to maximize your profit by choosing a single day to buy one stock
+# and choosing a different day in the future to sell that stock.
+# 
+# Return the maximum profit you can achieve from this transaction. If you
+# cannot achieve any profit, return 0.
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: prices = [7,1,5,3,6,4]
+# Output: 5
+# Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit =
+# 6-1 = 5.
+# Note that buying on day 2 and selling on day 1 is not allowed because you
+# must buy before you sell.
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: prices = [7,6,4,3,1]
+# Output: 0
+# Explanation: In this case, no transactions are done and the max profit =
+# 0.
+# 
+# 
+# 
+# Constraints:
+# 
+# 
+# 1 <= prices.length <= 10^5
+# 0 <= prices[i] <= 10^4
+# 
+# 
+#
+from typing import List
+# @lc code=start
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        min_price = float('inf')
+        max_profit = 0
+        for price in prices:
+            if price < min_price:
+                min_price = price
+            else:
+                max_profit = max(max_profit, price - min_price)
+        return max_profit
+        
+# @lc code=end
+
+s = Solution()
+prices = [7,1,5,3,6,4]
+print(s.maxProfit(prices))
