31733f5bbca53ffab532a0e1b12dedee473f19a8
gireeshsuresh <gireesh.star@gmail.com>
2025-09-30 19:43:48 -0400

    Misc

--- ProgrammingLanguages/Python/Sample.py
+++ ProgrammingLanguages/Python/Sample.py
@@ -1,6 +1,5 @@
 # /**
 #  * @author  [Gireesh Suresh]
-#  * @email   [gireesh.star@gmail.com]
 #  * @create  date 2020-04-06 18:51:33
 #  * @modify  date 2020-04-06 18:51:33
 #  * @desc    [Rough work]
